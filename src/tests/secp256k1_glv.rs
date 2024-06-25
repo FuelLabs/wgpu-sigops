45ffec97b7d07f5ad56f0b00d29ef1c5a63ca023
Koh Wei Jie <contact@kohweijie.com>
2024-06-25 13:25:51 +0800

    secp256k1 glv point mul test in arkworks

--- src/tests/secp256k1_glv.rs
+++ src/tests/secp256k1_glv.rs
@@ -10,9 +10,9 @@ use rand_chacha::ChaCha8Rng;
 #[tokio::test]
 pub async fn test_secp256k1_glv() {
     // Constants
-    let scalar_p = crate::moduli::secp256k1_fr_modulus_biguint();
-    let half_scalar_p = &scalar_p / BigUint::from(2u32);
-    let h = Fr::from(half_scalar_p);
+    let n = crate::moduli::secp256k1_fr_modulus_biguint();
+    let half_n = &n / BigUint::from(2u32);
+    let h = Fr::from(half_n);
     
     // Precomputed by Hal Finney
     //let lambda = BigUint::parse_bytes(b"5363ad4cc05c30e0a5261c028812645a122e22ea20816678df02967c1b23bd72", 16).unwrap();
@@ -22,25 +22,26 @@ pub async fn test_secp256k1_glv() {
     let beta = Fq::from(beta);
 
     let a1 = BigUint::parse_bytes(b"3086d221a7d46bcde86c90e49284eb15", 16).unwrap();
-    let b1 = &scalar_p - BigUint::parse_bytes(b"e4437ed6010e88286f547fa90abfe4c3", 16).unwrap();
+    let b1 = &n - BigUint::parse_bytes(b"e4437ed6010e88286f547fa90abfe4c3", 16).unwrap();
+    let neg_b1 = &n - &b1;
     let a2 = BigUint::parse_bytes(b"114ca50f7a8e2f3f657c1108d9d44cfd8", 16).unwrap();
-    let b2 = BigUint::parse_bytes(b"3086d221a7d46bcde86c90e49284eb15", 16).unwrap();
+    let b2 = a1.clone();
 
     let mut rng = ChaCha8Rng::seed_from_u64(2);
 
     for _ in 0..1000 {
         // Generate a random scalar k
-        let k: BigUint = rng.sample::<BigUint, RandomBits>(RandomBits::new(256)) % &scalar_p;
+        let k: BigUint = rng.sample::<BigUint, RandomBits>(RandomBits::new(256)) % &n;
 
         // Split k into k1 and k2
-        let c1 = (&b2 * &k) / &scalar_p;
-        let c2 = ((&scalar_p - &b1) * &k) / &scalar_p;
+        let c1 = (&b2 * &k) / &n;
+        let c2 = ((&neg_b1) * &k) / &n;
 
         let mut k1 = Fr::from(&k - &c1 * &a1 - &c2 * &a2);
-        let mut k2 = Fr::from((&scalar_p - &c1) * &b1 - &c2 * &b2);
+        let mut k2 = Fr::from((&n - &c1) * &b1 - &c2 * &b2);
 
         // Generate a random point
-        let r: BigUint = rng.sample::<BigUint, RandomBits>(RandomBits::new(256)) % &scalar_p;
+        let r: BigUint = rng.sample::<BigUint, RandomBits>(RandomBits::new(256)) % &n;
         let pt = Affine::generator().mul(Fr::from(r.clone())).into_affine();
         let mut p0 = pt;
 
